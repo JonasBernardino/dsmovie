9616f8b8106eb79a9f5696aa214459cc6f6f1722
jonasbernardino <jonas.bernardino@dcx.ufpb.br>
2022-01-15 00:14:26 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,11 @@
+import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
 
 export function Listing() {
   return (
     <>
       <Pagination />
+      <MovieScore />
     </>
   );
 }
